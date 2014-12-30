089b1d34f2b81303c16548477a72f3430db2522e
steven-gardiner <steven-gardiner>
2014-12-30 16:45:02 -0500

    typo

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,4 +1,4 @@
 espeak --version  || echo "espeak must be installed"
 (lame --version | head -1) || echo "lame must be installed"
-oggenv --version || echo "vorbis-tools must be installed"
+oggenc --version || echo "vorbis-tools must be installed"
 aplay --version || echo "alsa-utils must be installed"
